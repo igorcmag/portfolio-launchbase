9f7ab25c2814cc3ec724008d914bdb4a5b46f43b
Igor Caldeira <igorcmag@gmail.com>
2020-07-08 20:37:47 -0300

    personal updates

--- data.js
+++ data.js
@@ -1,41 +1,18 @@
 module.exports = [
     {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free",
-        featured: true
+        title: "Ecoleta", 
+        description: "Description here",
+        img_url: "https://i.ytimg.com/vi/YjRimhsofn8/maxresdefault.jpg",
+        website_url: "https://ecoleta-igorcmag.herokuapp.com/",
+        github_url: "https://github.com/igorcmag/Ecoleta",
+        incomplete: true
     },
     {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free"
-    },
-    {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free"
-    },
-    {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free",
-        featured: true
-    },
-    {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free"
-    },
-    {
-        id: "vqrjFnq3-uo",
-        title: "Formulário animado com JS puro e CSS", 
-        duration: "57 min",
-        price: "Free",
-        featured: true
+        title: "Portfolio", 
+        description: "Description here",
+        img_url: "https://i.ibb.co/vhfSWcJ/Whats-App-Image-2020-07-06-at-11-22-42-PM.jpg",
+        website_url: "/",
+        github_url: "https://github.com/igorcmag/portfolio-launchbase",
+        incomplete: true
     },
 ];
--- server.js
+++ server.js
@@ -18,12 +18,12 @@ server.get('/', function(req, res){
     const about = {
         avatar_url: "https://avatars0.githubusercontent.com/u/62859328?s=460&u=c96b4ff1cc9cbe13b6756b780fe9814587b932e8&v=4",
         name: "Igor Magalhães",
-        role: "Instrutor Rocketseat",
-        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes na programação. Colaborador da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        role: "Engineering Student @ ITA",
+        description: 'Third year undergraduate student. Passionate about Artificial Intelligence and Software Engineering. Learning Web Development at <a href="http://www.rocketseat.com.br" target="_blank"s>Rocketseat</a>.',
         links: [
-            {name: "Github", url: "/"},
-            {name: "Linkedin", url: "/"},
-            {name: "Lattes", url: "/"}
+            {name: "Github", url: "https://www.github.com/igorcmag"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/igorcmag"},
+            {name: "Lattes", url: "http://lattes.cnpq.br/2019990052192292"}
         ]
     };
     return res.render('about', {about});
@@ -45,6 +45,10 @@ server.get('/video', function(req, res){
         res.send('Video not found');
     }
     res.render('video', {item: video});
+});
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
 });
 
 server.listen(5002, function(){
